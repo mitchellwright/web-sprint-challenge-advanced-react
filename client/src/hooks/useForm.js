2e41554181e17d136fb4a3235d0bf1b4a12ef619
Mitchell Wright <mitchellbwright@gmail.com>
2020-08-14 17:27:30 -0600

    build custom useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+const useForm = () => {
+  const [state, setState] = useState({});
+
+  const handleChange = (e) => {
+    e.persist();
+    setState((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  return [state, handleChange];
+};
+
+export default useForm;

536f9ccc4b4742c6dccada497fcdfdc54f6f6c8b
Mitchell Wright <mitchellbwright@gmail.com>
2020-08-14 18:27:48 -0600

    complete success message test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -13,4 +13,20 @@ test("form header renders", () => {
   expect(header).toBeTruthy();
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+  // arrange
+  const { getByLabelText, getByText, getByTestId } = render(<CheckoutForm />);
+  const firstNameInput = getByLabelText("First Name:");
+
+  // act
+  fireEvent.change(firstNameInput, { target: { value: "Mitchell" } });
+  // submit form
+  fireEvent.click(getByTestId("checkout-button"));
+  const successMessage = await waitFor(() =>
+    getByText(/Your new green friends will be shipped to:/i)
+  );
+  const successFirstName = getByText("Mitchell");
+  // assert
+  expect(successMessage).toBeTruthy();
+  expect(successFirstName).toBeTruthy();
+});
